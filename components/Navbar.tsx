285eda75216fe5aa7755842aba1586ba8d299d5d
Balint Korosi <balint.j.korosi@gmail.com>
2023-02-14 16:00:41 +0100

    yolo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import { useTheme } from "next-themes";
+import { RiMoonFill, RiSunLine } from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
+
+const NAV_ITEMS = [
+  { label: "Home", page: "home" },
+  { label: "About", page: "about" },
+  { label: "Projects", page: "projects" },
+];
 
 const Navbar = () => {
   return <div>Navbar</div>;
