122bafa33a07542c5cc02667f51d25c575d6e371
Balint Korosi <balint.j.korosi@gmail.com>
2023-02-17 14:12:53 +0100

    added streak

--- components/GithubStreak.tsx
+++ components/GithubStreak.tsx
@@ -1,15 +1,17 @@
 function StreakStats() {
   return (
-    <iframe
-      className="bg-indigo-600"
-      // width="100%"
-      // height="300"
-      width="100%"
-      height="300"
-      scrolling="no"
-      frameBorder="no"
-      src={`https://streak-stats.demolab.com?user=korbal&theme=nightowl&hide_border=true&date_format=%5BY%20%5DM%20j&border_radius=0&stroke=dddddd`}
-    />
+    <div className="dark:bg-stone-900">
+      <iframe
+        // className="dark:bg-indigo-600"
+        // width="100%"
+        // height="300"
+        width="100%"
+        height="300"
+        scrolling="no"
+        frameBorder="no"
+        src={`https://streak-stats.demolab.com?user=korbal&theme=nightowl&hide_border=false&date_format=%5BY%20%5DM%20j&border_radius=5`}
+      />
+    </div>
   );
 }
 
