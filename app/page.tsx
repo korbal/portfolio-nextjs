68a12cfe9629843a9ed19cf1fdcea2e0e9f42478
Balint Korosi <balint.j.korosi@gmail.com>
2023-02-18 17:19:21 +0100

    yolo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
-import ProjectsSection from "@/components/ProjectsSection";
+// import HeroSection from "@/components/HeroSection";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import ProjectsSection from "../components/ProjectsSection";
 
 export default function Home() {
   return (
